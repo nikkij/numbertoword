ed0014285244887d889328e1a48680bb67f528a0
Nicole Jacobson <nikki@Nicoles-MacBook-Pro-2.local>
2014-08-22 22:31:20 +0700

    Add main num_to_word method and tests

--- lib/numbertoword.rb
+++ lib/numbertoword.rb
@@ -1,5 +1,47 @@
 require "numbertoword/version"
 
 module Numbertoword
+  class Convert
+    ONES = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    TEENS = ['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']   
+    TENS = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+    PARTS = [[1000000, 'million'], [1000, 'thousand'], [100, 'hundred'], [10, ''], [1, '']]
 
+    def num_to_word(n)
+      # Make sure input is an integer
+      unless n.class < Numeric
+        raise ArgumentError
+      end
+      
+      # Make sure input is in range
+      unless n.between?(1,999999999)
+      	raise ArgumentError
+      end
+
+      str = ""
+      PARTS.each do |part_integer, part_in_word|
+
+      	# Skip through the PARTS array to find the place to start
+        next if n < part_integer
+         
+        d = n / part_integer
+        n = n % part_integer
+
+        case part_integer
+        when 10
+          if d == 1 && n != 0
+          	str << TEENS[n-1]
+          else
+          	str << TENS[d-1] + " "
+          end
+        when 1
+          str << ONES[d]
+        else
+          str << "#{num_to_word(d)} #{part_in_word} " if d > 0
+        end
+      end
+      str
+    end
+
+  end
 end
--- spec/numbertoword_spec.rb
+++ spec/numbertoword_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe Numbertoword do
-  it "needs to be implemented"
+  before(:each) do
+    @converter = Numbertoword::Convert.new
+  end
+
+  it "should take a one digit number and return a word" do
+    expect(@converter.num_to_word(3)).to eq("three")
+  end
+
+  it "should take a two digit number and return words" do
+    expect(@converter.num_to_word(54)).to eq("fifty four")
+  end
+
+  it "should take a three digit number and return words" do
+    expect(@converter.num_to_word(123)).to eq("one hundred twenty three")
+  end
+
+  it "should take a four digit number and return words" do
+    expect(@converter.num_to_word(2345)).to eq("two thousand three hundred forty five")
+  end
+
+  it "should take a five digit number and return words" do
+    expect(@converter.num_to_word(45678)).to eq("forty five thousand six hundred seventy eight")
+  end
+
+  it "should take a six digit number and return words" do
+    expect(@converter.num_to_word(987654)).to eq("nine hundred eighty seven thousand six hundred fifty four")
+  end
+
+  it "should take a seven digit number and return words" do
+    expect(@converter.num_to_word(1234567)).to eq("one million two hundred thirty four thousand five hundred sixty seven")
+  end
+
+  it "should raise an ArgumentError when input is not numeric" do
+    expect{@converter.num_to_word("403")}.to raise_error("ArgumentError")
+  end
+
+  it "should raise an ArgumentError when input is out of range" do
+    expect{@converter.num_to_word(4000000000000)}.to raise_error("ArgumentError")
+  end  
 end
